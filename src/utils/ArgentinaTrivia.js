b2b43ca68954cd12af502a235e1871337752781a
fraancofenoglio <francofeno26@gmail.com>
2023-03-28 23:47:22 -0300

    primeras 10 preguntas sobre Argentina añadidas

---
+++ src/utils/ArgentinaTrivia.js
@@ -0,0 +1,52 @@
+export const ArgentinaTrivia = [
+    {
+        question: "¿Cuál es la capital de Argentina?",
+        answers: ["Córdoba", "Mendoza", "Buenos Aires", "Rosario"],
+        correct: "Buenos Aires"
+    },
+    {
+        question: "¿En qué año se independizó Argentina de España?",
+        answers: ['1828', '1821', '1810', '1816'],
+        correct: "1816"
+    },
+    {
+        question: "¿Cuál es el plato nacional de Argentina?",
+        answers:  ['Asado', 'Puré de papas', 'Paella', 'Sushi'],
+        correct: "Asado"
+    },
+    {
+        question: "¿Cuál es el río más ancho de Argentina?",
+        answers: ['Río de la Plata', 'Río Paraná', 'Río Paraguay', 'Río Salado'],
+        correct: "Río de la Plata"
+    },
+    {
+        question: "¿En qué año ganó Argentina su segunda Copa del Mundo?",
+        answers: ['1970', '1986', '1974', '1978'],
+        correct: "1986"
+    },
+    {
+        question: "¿Cuál es el ave nacional de Argentina?",
+        answers: ['El hornero', 'El cóndor', 'El flamenco', 'La golondrina'],
+        correct: "El hornero"
+    },
+    {
+        question: "¿Cuál es la montaña más alta de Argentina?",
+        answers: ["Monte Fitz Roy", "Aconcagua", "Cerro Torre", "Lanín"],
+        correct: "Aconcagua"
+    },
+    {
+        question: "¿Quién fue el líder del Ejército de los Andes y uno de los héroes de la independencia de Argentina?",
+        answers: ['José de San Martín', 'Simón Bolívar', "Bernardo O'Higgins", 'Manuel Belgrano'],
+        correct: "José de San Martín"
+    },
+    {
+        question: "¿En que provincia se encuentra la ciudad de Bariloche?",
+        answers: ['Chubut', 'La pampa', 'Río Negro', 'Córdoba'],
+        correct: "Río Negro"
+    },
+    {
+        question: "¿Cuál es la moneda oficial de Argentina?",
+        answers: ["Dólar Estadounidense", "Euro", "Peso argentino", "Yen"],
+        correct: "Peso argentino"
+    }
+]
